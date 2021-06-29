c681b59f14a10201312a27f0be52a230fe02dfe2
Austin Ray <austin@austinray.io>
2021-06-29 17:06:51 -0400

    nand2tetris: implement Mult.asm
    Add a naive implementation of Mult.asm that loops and adds R0 to itself
    x times. There's one optimization of early termination if either R0 or
    R1 is zero. There's potential to further optimize the multiplication.

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,36 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    @R2
+    M=0
+(LOOP)
+    // Assert R0 != 0
+    @R1
+    D=M
+    @END
+    D;JEQ
+
+    // Assert R1 != 0
+    @R1
+    D=M
+    @END
+    D;JEQ
+
+    // Load R0 for use in multiplication.
+    @R0
+    D=M
+
+    // Load R2 and add R0 to it.
+    @R2
+    M=M+D
+
+    // Load R1, decrement, and store in data register.
+    @R1
+    M=M-1
+    D=M
+
+    @LOOP
+    D;JGE
+(END)
+    @END
+    0;JMP
