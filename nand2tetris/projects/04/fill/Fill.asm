a72473b204f52cb2958cde60a23af08f0154ded7
Austin Ray <austin@austinray.io>
2021-06-30 00:25:57 -0400

    nand2tetris: complement Fill.asm
    Completed the Fill.asm assignment. It was very tedious work due to the
    nature of machine language.

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -2,13 +2,73 @@
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
-
+//
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(INIT_PTR)
+    @ptr
+    M=0
+
+(EVENT_LOOP)
+    @KBD
+    D=M
+
+    @BLACK
+    D;JNE
+
+    @WHITE
+    0;JMP
+
+(BLACK)
+    // Check if ptr exceeds screen memory range
+    @ptr
+    D=M
+    @8192 // (512x256) / 16
+    D=D-A
+    @INIT_PTR
+    D;JEQ
+
+    // Otherwise, update the screen.
+    @ptr
+    D=M
+
+    @SCREEN
+    A=A+D
+    M=-1
+
+    // Increment pointer to the next 16 characters.
+    @ptr
+    M=M+1
+
+    @BLACK
+    0;JMP
+
+(WHITE)
+    // Check if ptr exceeds screen memory range
+    @ptr
+    D=M
+    @8192 // (512x256) / 16
+    D=D-A
+    @INIT_PTR
+    D;JEQ
+
+    // Otherwise, update the screen.
+    @ptr
+    D=M
+
+    @SCREEN
+    A=A+D
+    M=0
+
+    // Increment pointer to the next 16 characters.
+    @ptr
+    M=M+1
+
+    @WHITE
+    0;JMP
